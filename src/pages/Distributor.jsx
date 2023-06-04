ead91cda2d244778535f62c73103ccd0db26a743
thejavidev <javidation.l@gmail.com>
2023-06-04 21:59:21 +0400

    beta-4

--- src/pages/Distributor.jsx
+++ src/pages/Distributor.jsx
@@ -16,12 +16,14 @@ const Distributor = ({ data }) => {
   const distribution = data?.distrubition;
 
   const { id } = useParams();
+  const { distribusiya_one_id } = useParams();
   const currentPost = distribution?.find((post) => post?.id == id);
   const currentItem = currentPost?.sub_categories
 
-  const currentPost2 = currentItem?.find((post) => post?.distribusiya_one_id == id);
+  const currentPost2 = currentItem?.find((post) => post?.distribusiya_two_id == distribusiya_one_id);
   const currentItem2 = currentPost2?.products;
 
+ 
 
   return (
     <>
@@ -37,7 +39,7 @@ const Distributor = ({ data }) => {
             <h2 className='absolute top-[65%] left-[50%] z-5 text-[--textfff] text-[40px] font-[200] capitalize transfrom70'>{t("distribution")}</h2>
           </div>
         </div>
-        <section id="1" className='pt-[60px] bg-[--bgef] min-h-[55vh] pb-[60px] pl-[50px] pr-[50px] lg:pl-[20px] lg:pr-[20px] relative'>
+        <section id="1" className='pt-[60px] bg-[--bgef] min-h-[155vh] pb-[60px] pl-[50px] pr-[50px] lg:pl-[20px] lg:pr-[20px] relative'>
           <Container fluid >
             <Row className=''>
               {
@@ -49,39 +51,39 @@ const Distributor = ({ data }) => {
                   </Col>
                 ))
               }
-              <Tabs className='mt-[20px]'>
-                <TabList className='grid gap-3 grid-rows-6 md:grid-rows-4 xs:grid-rows-2 border-undisplay'>
-                  {
-                    currentItem && currentItem?.map((cur, i) => (
-                      <div className='bg-[--textfff]' key={i} >
-                        <Tab _selected={{ color: 'white', bg: 'blue.500' }}>
-                          <h2 className=''>{ml(cur?.name_az, cur?.name_ru, cur?.name_en)}</h2>
-                        </Tab>
-                      </div>
-                    ))
-                  }
-                </TabList>
-                <TabPanels>
+              <div className='mt-[20px]'>
+                {
+                  currentItem && currentItem?.map((cur, i) => (
+                    <div className='' key={i} >
+                      <NavLink className='bg-[--textfff]'>
+                        <h2 className=''>{ml(cur?.name_az, cur?.name_ru, cur?.name_en)}</h2>
+                      </NavLink>
+                    </div>
+                  ))
+                }
+                <Tabs>
                   {
                     currentItem2 && currentItem2?.map((cur, i) => (
-                      <TabPanel key={i} className='tab2n flex overflow-hidden flex-wrap pt-[60px] pb-[60px] pl-[100px] pr-[100px]'>
-                          <Row>
-                            <Col lg={8}>
-                                <div className="flex flex-col justify-evenly h-full pt-0 pb-0 pr-[50px] pl-0">
-                                    <h2 className='font-[600] text-[40px] text-[--text] pb-[30px]'>{ml(cur?.title_az,cur?.title_ru,cur?.title_en)}</h2>
-                                    <div dangerouslySetInnerHTML={{ __html: cur?.text_az && ml(cur?.text_az,cur?.text_ru,cur?.text_en) }}></div>
-                                </div>
-                            </Col>
-                            <Col lg={4}>
-                              <LazyLoadImage src={cur?.src} />
-                            </Col>
-                          </Row>
-                      </TabPanel>
+                      <TabList key={i} className='tab2n flex overflow-hidden flex-wrap pt-[60px] pb-[60px] pl-[100px] pr-[100px]'>
+                        <Tab>
+                          <Col lg={8}>
+                            <div className="flex flex-col justify-evenly h-full pt-0 pb-0 pr-[50px] pl-0">
+                              <h2 className='font-[600] text-[40px] text-[--text] pb-[30px]'>{ml(cur?.title_az, cur?.title_ru, cur?.title_en)}</h2>
+                              <div dangerouslySetInnerHTML={{ __html: cur?.text_az && ml(cur?.text_az, cur?.text_ru, cur?.text_en) }}></div>
+                            </div>
+                          </Col>
+                          <Col lg={4}>
+                            <LazyLoadImage src={cur?.src} />
+                          </Col>
+                        </Tab>
+
+                      </TabList>
+
                     ))
                   }
 
-                </TabPanels>
-              </Tabs>
+                </Tabs>
+              </div>
             </Row>
           </Container>
         </section>
